2d737d5439599f06a4a44d957e8cb0891813caef
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-09-20 19:39:45 +0300

    Add Teledoc.SharedKernel.csproj with defined dependencies and build configurations

--- Teledoc.SharedKernel/ValueObject.cs
+++ Teledoc.SharedKernel/ValueObject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Teledoc.SharedKernel
+﻿namespace Teledoc.SharedKernel
 {
 	public abstract class ValueObject : IValueObject
 	{
