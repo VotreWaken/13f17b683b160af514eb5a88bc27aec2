319f1544da1070799a1bdec83c066e73cbb04204
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-09-20 19:39:30 +0300

    feat: Infrastructure.Add Configuration, DataContext, Enteties, Repository

--- Teledoc.Infrastructure/Configuration/ClientConfiguration.cs
+++ Teledoc.Infrastructure/Configuration/ClientConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Teledoc.Infrastructure.Configuration
+﻿namespace Teledoc.Infrastructure.Configuration
 {
 	public class ClientConfiguration
 	{
